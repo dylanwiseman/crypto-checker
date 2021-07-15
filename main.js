41a4ad8812615095f31f476b4a64a7bc61eb1143
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-07-15 12:11:26 -0600

    adding server

--- main.js
+++ main.js
@@ -35,18 +35,18 @@ const submitTicker = (event) => {
     })
     document.querySelector("input").value = ""
     // This is the random pokemon generator:
-    // axios.get(`https://pokeapi.co/api/v2/pokemon?limit=251`).then((res) => {
-    //     randomIndex = Math.floor(Math.random()*res.data.results.length)
-    //     console.log(randomIndex)
-    //     randomPokemon = res.data.results[randomIndex].name
-    //     console.log(randomPokemon)
-    //     let pokemonLink = document.createElement('a')
-    //     let pokemonImg = document.createElement('img')
-    //     pokemonLink.href = `href=http://pokemondb.net/pokedex/${randomPokemon}`
-    //     pokemonImg.src = `https://img.pokemondb.net/sprites/black-white/anim/normal/${randomPokemon}.gif`
-    //     div.appendChild(pokemonLink)
-    //     pokemonLink.appendChild(pokemonImg)
-    // })
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=251`).then((res) => {
+        randomIndex = Math.floor(Math.random()*res.data.results.length)
+        console.log(randomIndex)
+        randomPokemon = res.data.results[randomIndex].name
+        console.log(randomPokemon)
+        let pokemonLink = document.createElement('a')
+        let pokemonImg = document.createElement('img')
+        pokemonLink.href = `href=http://pokemondb.net/pokedex/${randomPokemon}`
+        pokemonImg.src = `https://img.pokemondb.net/sprites/black-white/anim/normal/${randomPokemon}.gif`
+        div.appendChild(pokemonLink)
+        pokemonLink.appendChild(pokemonImg)
+    })
 }
 
 cryptoBtn.addEventListener('click',submitTicker)
